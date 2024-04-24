b560382594408b2cd119a310b7ef7a41e5cbbf7c
radek-czcz <radowiczek@gmail.com>
2024-04-24 21:09:00 +0200

    add datetime of last scrap to webpage

--- react/appno1/src/App.js
+++ react/appno1/src/App.js
@@ -6,12 +6,13 @@ export default function App() {
   const [exams, setExams] = useState('');
   const [bPlan, setbPlan] = useState('');
   const [hPlan, sethPlan] = useState('');
+  const [lastTime, setLastTime] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8016/sprawdziany')
       // .then(resp => {setElem1(resp[1].htmlElement)})
       .then(resp => resp.json())
-      .then(res => {setExams(res[res.length-1].htmlElement); console.log('lenght: ', res.length)})
+      .then(res => {setExams(res[res.length-1].htmlElement); setLastTime(res[res.length-1].date + ' ' + res[res.length-1].time); console.log('lenght: ', res.length)})
       .catch(error => console.error('this error: ', error));
     fetch('http://localhost:8016/bPlan')
       // .then(resp => {setElem1(resp[1].htmlElement)})
@@ -27,12 +28,16 @@ export default function App() {
       setExams('');
       setbPlan('');
       sethPlan('');
+      setLastTime('');
     }
   }, []);
 
   return (
     <div className="mainComponent">
-      <div className='myMain' dangerouslySetInnerHTML={{__html: exams}}>
+      <div>
+        <p>{lastTime}</p>
+        <div className='myMain' dangerouslySetInnerHTML={{__html: exams}}>
+        </div>
       </div>
       <div className='myMain' dangerouslySetInnerHTML={{__html: bPlan}}>
       </div>
