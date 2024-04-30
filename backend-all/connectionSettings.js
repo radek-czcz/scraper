7db7a427b3c3d461cd398461b6f2c77061b48a2d
radek-czcz <radowiczek@gmail.com>
2024-04-30 19:20:54 +0200

    remove connectionSettings files

--- backend-all/connectionSettings.js
+++
@@ -1,9 +0,0 @@
-
-let settings = {
-    host: '188.210.222.87',
-    user: 'srv59554_mojeprodukty',
-    password: '2!gTaC83GwseD',
-    database: 'srv59554_mojeprodukty'
-  }
-
- module.exports = { settings }
