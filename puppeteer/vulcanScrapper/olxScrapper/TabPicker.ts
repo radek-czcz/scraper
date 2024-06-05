2a9094881ca8858dfd9760c4fa3519bdabdb797e
radek-czcz <radowiczek@gmail.com>
2024-06-05 20:27:21 +0200

    Make tab-selecting component

---
+++ puppeteer/vulcanScrapper/olxScrapper/TabPicker.ts
@@ -0,0 +1,23 @@
+import {Browser, Page, ElementHandle} from 'puppeteer'
+import { getBrowserFromParentProcess, getPageWithSelect } from '../puppLoader';
+const log = require('why-is-node-running');
+
+let br:Promise<Browser> = getBrowserFromParentProcess();
+let pa:Promise<Page>/* = br.then(() => getPageWithSelect());*/
+let browser:Browser;
+
+export function getThePageTab(carBrand:string):Promise<Page> {
+
+	let allTabs:Page[];
+	let prArray:Promise<Page> = br.then((res:Browser) => {browser = res; return res.pages()}).then((tabs:Page[]) => {
+		allTabs = tabs
+		return tabs.map((tab:Page) => tab.title())
+	}).then((res:Promise<string>[]) => Promise.all(res).then((arr:string[]) => {
+		let ind:number = arr.findIndex((inp:string) => inp.toLowerCase().includes(carBrand))
+		console.log('returning tab: ', arr[ind] );
+		browser.disconnect();
+		return allTabs[ind]
+	}))
+	return prArray
+}
+
