5594d2c8fbf109847685b6663c8b06d7a57121fa
radek-czcz <radowiczek@gmail.com>
2025-02-09 17:46:09 +0100

    write first basic tests for BrowserSubClass

--- spec/BrowserSubClass.spec.ts
+++ spec/BrowserSubClass.spec.ts
@@ -1,11 +1,37 @@
 import { BrowserSubClass } from "../puppeteer/BrowserGenerator/BrowserSubClass";
 import { Browser } from 'puppeteer';
 import "jasmine";
+import net, {Server, Socket} from 'net';
 
-describe('suite should test BrowserSubClass', function() {
-	it('Function launchBrowser() should return puppeteer.Browser instance', async function() {
-		let br2 = new BrowserSubClass();
-		let br = await br2.launchBrowser()
-		expect(typeof br === 'Browser').toBeTrue();
+describe('This suite should test BrowserSubClass\n', function() {
+
+	let br:Browser;
+	let br2:BrowserSubClass;
+
+	beforeEach(async function(){
+		br2 = await new BrowserSubClass();
+		br = await br2.browser
+	}, 10000)
+
+	it("Function launchBrowser() should return object with properties 'pages' and 'wsEndpoint'", function() {
+		expect('pages' in br && 'wsEndpoint' in br).toBeTrue();
+	})
+
+	it(`This function should make a net communication server on port 8088\n
+		and respond with sending a puppeteer browser wsEndpoint address`, function() {
+		expect(br2.establishNetServer).not.toThrow();
+		let endpoint:string;
+	  	let connect:Promise<Socket> = new Promise((resFunc, rejFunc) => {
+		    let client:Socket = net.connect({port: 8088}, function() {
+		      console.log('net.child says: connected to server!');  
+		    });
+
+		    client.on('data', function(data) {
+		      console.log('net.child says: data received - ', data.toString());
+		      endpoint = data.toString();
+		      client.end();
+		      resFunc(client);
+		    });
+		})
 	})
 })
