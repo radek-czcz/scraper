45bccc7a9861d55fb57f59a5227473937031fcf7
radek-czcz <radowiczek@gmail.com>
2025-03-16 16:07:35 +0100

    Optimize CookiesManager

--- puppeteer/BrowserGenerator/CookiesManager.ts
+++ puppeteer/BrowserGenerator/CookiesManager.ts
@@ -1,16 +1,10 @@
-// import { getPages } from '../../BrowserGenerator/PuppeteerBrowserOperations/puppLoader';
-// import process from 'node:process';
-// import { argv } from 'node:process';
-import fs from 'fs';
-import { writeFile, readFile } from 'node:fs/promises';
 import {Browser, Page, JSONArray, HTTPResponse} from 'puppeteer';
 import {ExistingBrowserSubClass} from './ExistingBrowserSubClass'
 import StorageDataReader from './Cookies/StorageDataReader'
 import StorageDataInserter from './Cookies/StorageDataInserter'
 import CookiesReader from './Cookies/CookiesReader'
 
-
-export class CookiesSetter {
+export class CookiesManager {
 
 	private page:Promise<Page>;
 
@@ -18,12 +12,12 @@ export class CookiesSetter {
 		this.page = page;
 	}
 
-	public setCookies():Promise<null | HTTPResponse> {
+	public setCookies(path:string):Promise<null | HTTPResponse> {
 
-		let cookies:Promise<JSONArray> = CookiesReader.getCookies('../ConfigFiles/vulcan/cookies.json');
+		let cookies:Promise<JSONArray> = CookiesReader.getCookies(path+'cookies.json');
 
 		let storageDataRead:(Promise<[JSONArray, JSONArray]>) = new StorageDataReader(
-			["../ConfigFiles/vulcan/local.json", "../ConfigFiles/vulcan/session.json"]
+			[path+'local.json', path+'session.json']
 		).readStorageData();
 
 		let storageDataInsert:StorageDataInserter = new StorageDataInserter(this.page);
@@ -39,15 +33,15 @@ export class CookiesSetter {
 		let insertCookies = Promise.all([
 			cookies,
 			this.page
-		]).then((res:[JSONArray, Page]) => res[1].setCookie(...<any>res[0]))
+		]).then((res:[JSONArray, Page]) => res[1].setCookie(...<any>res[0]));
 
 		let reload = Promise.all([
 			insertStorage,
 			insertCookies
 		])
-		.then(() => reloadPage())
+		.then(() => reloadPage());
 
-		return reload
+		return reload;
 	}
 }
 
@@ -57,9 +51,9 @@ let br:Promise<Browser> = ebs.browser
 
 let tab:Promise<Page> = br
 .then((browser:Browser) => browser.pages())
-.then((tabs:Page[]) => tabs[0])
+.then((tabs:Page[]) => tabs[0]);
 
-let cs:CookiesSetter = new CookiesSetter(tab)
+let cs:CookiesSetter = new CookiesSetter(tab);
 Promise.all([br, tab])
-.then((res:[Browser, Page]) => Promise.all([cs.setCookies(), br]))
+.then((res:[Browser, Page]) => Promise.all([cs.setCookies('../ConfigFiles/vulcan/'), br]))
 .then((br2:[HTTPResponse | null, Browser]) => br2[1].disconnect());
