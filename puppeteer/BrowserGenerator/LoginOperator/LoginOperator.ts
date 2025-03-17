c62dd7158eb93c2da6dffcfa87783bf72f240ca6
radek-czcz <radowiczek@gmail.com>
2025-03-17 20:36:12 +0100

    enhance LoginOperator

--- puppeteer/BrowserGenerator/LoginOperator/LoginOperator.ts
+++ puppeteer/BrowserGenerator/LoginOperator/LoginOperator.ts
@@ -1,26 +1,51 @@
-import { Page } from 'puppeteer'
+import {Browser, Page, JSONArray, HTTPResponse} from 'puppeteer';
+import {ExistingBrowserSubClass} from '../ExistingBrowserSubClass'
+import StorageDataReader from '../Cookies/StorageDataReader'
+import StorageDataInserter from '../Cookies/StorageDataInserter'
+import CookiesReader from '../Cookies/CookiesReader'
 
 export default class LoginOperator {
 
 	private page:Promise<Page>;
 	private credentials:[string, string];
-	private button:HTMLElement;
 
-	constructor(page:Promise<Page>, credentials:[/*user*/string, /*password*/string], button:HTMLElement) {
+	constructor(page:Promise<Page>, credentials:[/*user*/string, /*password*/string]) {
 		this.page = page
 		this.credentials = credentials
-		this.button = button;
 	}
 
-	writeLogin(selector:string) {
-
+	async writeLogin(selector:string):Promise<void> {
+		let page = await this.page;
+		page.waitForSelector(selector);
+		return page.type(selector, this.credentials[0], {delay: 100});
 	}
 
-	writePassword(selector:string) {
-
+	async writePassword(selector:string):Promise<void> {
+		let page = await this.page
+		page.waitForSelector(selector);
+		return page.type(selector, this.credentials[1], {delay: 100})
 	}
 
-	clickLogin() {
-
+	async clickNext(selector:string):Promise<void> {
+		let page = await this.page;
+		page.waitForSelector(selector);
+		return page.click(selector);
 	}
 }
+
+let ebs:ExistingBrowserSubClass = new ExistingBrowserSubClass();
+
+let br:Promise<Browser> = ebs.browser
+
+let tab:Promise<Page> = br
+.then((browser:Browser) => browser.pages())
+.then((tabs:Page[]) => tabs[0]);
+
+let lo:LoginOperator = new LoginOperator(tab, ['AAABOLQ-002234', '']);
+Promise.all([br, tab])
+.then((res:[Browser, Page]) => Promise.all([lo.writeLogin('input#Login'), br]))
+.then((res:[void, Browser]) => Promise.all([lo.clickNext('button#btNext'), res[1]]))
+// .then((res:[Browser, Page]) => Promise.all([lo.writePassword('input#Haslo'), br]))
+// .then((res:[Browser, Page]) => Promise.all([lo.clickLogin('button#btNext'), br]))
+// .then((br2:[void, Browser]) => br2[1].disconnect());
+.then((br2:[void, Browser]) => br2[1].disconnect());
