130750ccb01e92367f16af9c5b0e3d084eff28f0
radek-czcz <radowiczek@gmail.com>
2025-08-17 16:04:15 +0200

    make one new injection in Navigator

--- puppeteer/BrowserGenerator/Navigator.ts
+++ puppeteer/BrowserGenerator/Navigator.ts
@@ -6,12 +6,13 @@ export class Navigator {
 
 	constructor(
 		private page:Promise<Page>,
-		@inject('nav-time') private timeout:PuppeteerLifeCycleEvent
+		@inject('nav-time') private timeout:PuppeteerLifeCycleEvent,
+		@inject('nav-destination') private destinationUrl:string,
 	) {}
 
-	public goToPage(url:string):Promise<Page> {
+	public goToPage():Promise<Page> {
 		return this.page.then((page:Page) => 
-		  page.goto(url, {waitUntil: this.timeout})
+		  page.goto(this.destinationUrl, {waitUntil: this.timeout})
 		  .then(() => {console.log('navigate: success'); return page})
 		  .catch(err => {
 		  	console.log(`browser could not navigate to the page address\n${err}`);
